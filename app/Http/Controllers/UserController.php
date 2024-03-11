b40607f69ddf2fd83a93fdf12454b9a0181af19c
Mystrium <godunokanton@gmail.com>
2024-03-11 13:31:21 +0200

    tests done

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
@@ -15,7 +16,9 @@ class UserController extends Controller
         //   order by created_at desc
         //   limit 3
 
-        $users = User::all(); // replace this with Eloquent statement
+        $users = User::whereNotNull('email_verified_at')
+            ->orderBy('created_at', 'desc')
+            ->limit(3)->get(); // replace this with Eloquent statement
 
         return view('users.index', compact('users'));
     }
@@ -23,7 +26,9 @@ class UserController extends Controller
     public function show($userId)
     {
         $user = NULL; // TASK: find user by $userId or show "404 not found" page
-
+        if($user == null)
+            abort(404);
+        
         return view('users.show', compact('user'));
     }
 
@@ -32,6 +37,7 @@ class UserController extends Controller
         // TASK: find a user by $name and $email
         //   if not found, create a user with $name, $email and random password
         $user = NULL;
+        $user = User::firstOrCreate(['name' => $name, 'email' => $email], ['password' => Hash::make(rand(10000000, 99999999))]);
 
         return view('users.show', compact('user'));
     }
@@ -41,6 +47,7 @@ class UserController extends Controller
         // TASK: find a user by $name and update it with $email
         //   if not found, create a user with $name, $email and random password
         $user = NULL; // updated or created user
+        $user = User::updateOrCreate(['name' => $name], ['email' => $email, 'password' => Hash::make(rand(10000000, 99999999))]);
 
         return view('users.show', compact('user'));
     }
@@ -52,6 +59,7 @@ class UserController extends Controller
         // $request->users is an array of IDs, ex. [1, 2, 3]
 
         // Insert Eloquent statement here
+        User::whereIn('id', $request->users)->delete();
 
         return redirect('/')->with('success', 'Users deleted');
     }
--- app/Models/User.php
+++ app/Models/User.php
@@ -2,7 +2,7 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -12,6 +12,7 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
+    protected $table = 'users';
     /**
      * The attributes that are mass assignable.
      *
@@ -38,7 +39,9 @@ class User extends Authenticatable
      *
      * @var array
      */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
+    protected $casts = ['email_verified_at' => 'datetime',];
+
+    public function scopeActive(Builder $query){
+        return $query->whereNotNull('email_verified_at');
+    }
 }
